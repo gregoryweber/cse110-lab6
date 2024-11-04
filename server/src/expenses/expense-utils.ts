398fa3b747fcd9e53d60ec09e5b7ae0f5b179f65
gregoryweber <gregory1@weberdao.org>
2024-11-04 13:42:25 -0800

    GET expenses done

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -2,26 +2,34 @@ import { Database } from "sqlite";
 import { Expense } from "../types";
 import { Request, Response } from "express";
 
-export async function createExpenseServer(req: Request, res: Response, db: Database) {
-
-    try {
-        // Type casting the request body to the expected format.
-        const { id, cost, description } = req.body as { id: string, cost: number, description: string };
- 
-        if (!description || !id || !cost) {
-            return res.status(400).send({ error: "Missing required fields" });
-        }
- 
-        await db.run('INSERT INTO expenses (id, description, cost) VALUES (?, ?, ?);', [id, description, cost]);
-        res.status(201).send({ id, description, cost });
- 
-    } catch (error) {
- 
-        return res.status(400).send({ error: `Expense could not be created, + ${error}` });
+export async function createExpenseServer(
+  req: Request,
+  res: Response,
+  db: Database
+) {
+  try {
+    // Type casting the request body to the expected format.
+    const { id, cost, description } = req.body as {
+      id: string;
+      cost: number;
+      description: string;
     };
- 
- }
- 
+
+    if (!description || !id || !cost) {
+      return res.status(400).send({ error: "Missing required fields" });
+    }
+
+    await db.run(
+      "INSERT INTO expenses (id, description, cost) VALUES (?, ?, ?);",
+      [id, description, cost]
+    );
+    res.status(201).send({ id, description, cost });
+  } catch (error) {
+    return res
+      .status(400)
+      .send({ error: `Expense could not be created, + ${error}` });
+  }
+}
 
 export function deleteExpense(
   req: Request,
@@ -39,6 +47,8 @@ export function deleteExpense(
   res.status(204).send();
 }
 
-export function getExpenses(req: Request, res: Response, expenses: Expense[]) {
-  res.status(200).send({ data: expenses });
+export async function getExpenses(req: Request, res: Response, db: Database) {
+  const response = await db.all("SELECT * FROM expenses");
+  console.log(response);
+  res.status(200).send({ data: response });
 }
